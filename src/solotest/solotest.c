c61589e1fc3ef254b75077006793e1f82b796995
metwse <108795071+metwse@users.noreply.github.com>
2025-07-20 18:28:31 +0300

    feat: solotest_move

--- src/solotest/solotest.c
+++ src/solotest/solotest.c
@@ -1,4 +1,5 @@
 #include "../../include/solotest.h"
+#include "../../include/solotest_ext.h"
 
 #include <stdint.h>
 
@@ -10,6 +11,16 @@ void solotest_init(struct solotest_board *restrict b) {
 	b->pegs_can_move[1] = 0x4000; // index 14
 	b->pegs_can_move[2] = 0x10; // index 4
 	b->pegs_can_move[3] = 0x40000; // index 18
+}
+
+void solotest_clone(struct solotest_board *dst,
+		    const struct solotest_board *src) {
+	dst->peg_count = src->peg_count;
+	dst->board = src->board;
+	dst->pegs_can_move[0] = src->pegs_can_move[0];
+	dst->pegs_can_move[1] = src->pegs_can_move[1];
+	dst->pegs_can_move[2] = src->pegs_can_move[2];
+	dst->pegs_can_move[3] = src->pegs_can_move[3];
 }
 
 // See https://graphics.stanford.edu/~seander/bithacks.html#ZerosOnRightBinSearch
@@ -48,4 +59,90 @@ inline int solotest_bitfield_to_index(uint64_t bitfield) {
 
 inline uint64_t solotest_index_to_bitfield(int index) {
 	return (uint64_t) 1 << index;
+}
+
+
+static inline uint64_t set_board_index(uint64_t board, int index)
+{
+	return index == -1 ? board : board | solotest_index_to_bitfield(index);
+}
+
+static inline uint64_t clear_board_index(uint64_t board, int index)
+{
+	return index == -1 ? board : board & ~solotest_index_to_bitfield(index);
+}
+
+static inline uint64_t get_board_index(uint64_t board, int index)
+{
+	return index == -1 ? 0 : board & solotest_index_to_bitfield(index);
+}
+
+
+
+static inline void place_peg(struct solotest_board *b, int index) {
+	b->board = set_board_index(b->board, index);
+}
+static inline void block_move_to(struct solotest_board *b, int index) {
+	struct solotest_neighbor_indexes farn = solotest_get_far_neighbors(index);
+	b->pegs_can_move[0] = clear_board_index(b->pegs_can_move[0], farn.bottom);
+	b->pegs_can_move[1] = clear_board_index(b->pegs_can_move[1], farn.left);
+	b->pegs_can_move[2] = clear_board_index(b->pegs_can_move[2], farn.top);
+	b->pegs_can_move[3] = clear_board_index(b->pegs_can_move[3], farn.right);
+}
+
+static inline void clear_peg(struct solotest_board *b, int index) {
+	b->board = clear_board_index(b->board, index);
+}
+
+static inline void free_to_move(struct solotest_board *b, int index) {
+	struct solotest_neighbor_indexes farn = solotest_get_far_neighbors(index);
+	struct solotest_neighbor_indexes n = solotest_get_neighbors(index);
+
+	if (get_board_index(b->board, n.bottom))
+		b->pegs_can_move[0] = set_board_index(b->pegs_can_move[0], farn.bottom);
+	if (get_board_index(b->board, n.left))
+		b->pegs_can_move[1] = set_board_index(b->pegs_can_move[1], farn.left);
+	if (get_board_index(b->board, n.top))
+		b->pegs_can_move[2] = set_board_index(b->pegs_can_move[2], farn.top);
+	if (get_board_index(b->board, n.right))
+		b->pegs_can_move[3] = set_board_index(b->pegs_can_move[3], farn.right);
+}
+
+
+void solotest_move(struct solotest_board *b, int peg_index,
+		   enum solotest_direction d) {
+	struct solotest_neighbor_indexes farn =
+		solotest_get_far_neighbors(peg_index);
+	struct solotest_neighbor_indexes n =
+		solotest_get_neighbors(peg_index);
+
+	int clear;
+	int place;
+
+	switch (d) {
+	case SOLOTEST_DIRECTION_TOP:
+		clear = n.top;
+		place = farn.top;
+		break;
+	case SOLOTEST_DIRECTION_RIGHT:
+		clear = n.right;
+		place = farn.right;
+		break;
+	case SOLOTEST_DIRECTION_BOTTOM:
+		clear = n.bottom;
+		place = farn.bottom;
+		break;
+	case SOLOTEST_DIRECTION_LEFT:
+		clear = n.left;
+		place = farn.left;
+		break;
+	}
+
+	clear_peg(b, peg_index);
+	clear_peg(b, clear);
+	place_peg(b, place);
+
+	free_to_move(b, peg_index);
+	free_to_move(b, clear);
+	block_move_to(b, place);
 }
